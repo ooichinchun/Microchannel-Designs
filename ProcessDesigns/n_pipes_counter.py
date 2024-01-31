6abf6be70802cb592ed2444e1c6f10c571cb17d7
ooichinchun <ooichinchun@users.noreply.github.com>
2024-01-31 16:56:25 +0800

    Update n_pipes_counter.py
    Fixed logic error when pipes touch or are near top or bottom edges

--- ProcessDesigns/n_pipes_counter.py
+++ ProcessDesigns/n_pipes_counter.py
@@ -42,6 +42,12 @@ def inlet_pipes_counter(folder, to_save_folder):
     file_list = [f for f in os.listdir(folder)]
     file_list.sort(key=extract_case_number)  # Sort case numbers in ascending order
 
+    ok = True #Logic to determine if there are any 0 pipes    
+    count_1 = 0
+    count_2 = 0
+    count_3 = 0
+    count_4 = 0
+
     for num, file_name in enumerate(file_list):
         file_path = os.path.join(folder, file_name)
 
@@ -54,19 +60,41 @@ def inlet_pipes_counter(folder, to_save_folder):
         resized_image = pil_image.resize((64,64), resample=Image.BICUBIC) #Resize to (64,64) to match diffusion model input
 
         # Convert the resized image to binary image
-        image_array = np.array(resized_image) > 128  # Thresholding to get binary image (1 and 0)
+        image_array = (~(np.array(resized_image) > 128)).astype(int) # Thresholding to get binary image (1 and 0)
 
-        # Count number of times switching from 0 to 1 and vice versa
-        switch_count = int(np.round(np.sum(image_array[:-1, 0] != image_array[1:, 0]) / 2))
+        #Count number of inlet pipes
+        edge = 0 #Initiailise counter to count number of edge cases (where pipe touches top or bottom walls)
 
-        result_array = np.full((64, 64, 1), switch_count, dtype=int)
+        if image_array[0, 0] == 1: #If pipe touches top wall
+            edge += 1
+
+        if image_array[-1, 0] == 1: #If pipe touches bottom wall
+            edge += 1
+
+        switches = np.sum(image_array[:-1, 0] != image_array[1:, 0]) #Number of value switches between 0 and 1
+        num_pipes = int((switches+edge)/2) #Factor in edge situations and obtain the right number of inlet pipes
+
+        if num_pipes == 0:
+            ok = False
+        elif num_pipes == 1:
+          count_1 += 1
+        elif num_pipes == 2:
+          count_2 += 1
+        elif num_pipes == 3:
+          count_3 += 1
+        elif num_pipes == 4:
+          count_4 += 1
+	    
+        result_array = np.full((64, 64, 1), num_pipes, dtype=int)
         filename = f'num_pipe_{num}.npy'
         output_path = os.path.join(to_save_folder, filename)
         np.save(output_path, result_array)
 
         # Print processing information
-        print(f'Done processing {file_name}. Value switch count: {switch_count}')
-
+        print(f'Done processing {file_name}. Value switch count: {num_pipes}')
+    
+    print('Ok?', ok) #To report if no 0 pipes found
+    print('Number of 1 pipes: ', count_1, 'Number of 2 pipes: ', count_2, 'Number of 3 pipes: ', count_3, 'Number of 4 pipes: ', count_4)
 
 inlet_pipes_counter(folder_path, output_folder)
 
